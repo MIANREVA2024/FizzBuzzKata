8e9039527ba8163f681e458c43d09852ef55a9d7
MIANREVA2024 <miguel.reyes.vasquez@gmail.com>
2024-11-05 12:27:05 +0100

    Desarrollando el Fizzbuzz

---
+++ src/main/java/org/vir/Fizzbuzz.java
@@ -0,0 +1,25 @@
+package org.vir;
+
+public class Fizzbuzz {
+    public String checkNumber(int number) {
+        if (number % 3 == 0){
+            return "Fizz";
+        }
+        if (number % 5 == 0){
+            return "Buzz";
+        }
+        if (number % 3 == 0 && number % 5 == 0){
+            return "FizzBuzz";
+        }
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+
+        if (number % 5 == 0) {
+            return "Buzz";
+        }
+        return String.valueOf(number);
+
+    }
+    
+}
